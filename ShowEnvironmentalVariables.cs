05017dd5f7ece117c0b1805413bb47ef8e81be3e
Josh Owen <wclax04@gmail.com>
2010-01-14 21:13:12 -0500

    Added ShowEnvironmentalVariables.cs This file shows all environmental variables and other information when placed in a .net WebApplication

---
+++ ShowEnvironmentalVariables.cs
@@ -0,0 +1,25 @@
+protected void Page_Load(object sender, EventArgs e)
+       {
+           IDictionary variables = Environment.GetEnvironmentVariables();
+           string[] drives = Environment.GetLogicalDrives();
+           Response.Write("<strong>Environmentals</strong> <br>");
+           foreach (DictionaryEntry var in variables)
+           {
+               Response.Write(var.Key + "~" + var.Value + "<br>");
+           }
+           Response.Write("<strong>Drives</strong> <br>");
+           foreach (string drive in drives)
+           {
+               Response.Write(drive + "<br>");
+           }
+           Response.Write("<strong>AppSettings</strong> <br>");
+           foreach (DictionaryEntry setting in ConfigurationManager.AppSettings)
+           {
+               Response.Write(setting.Key + "~" + setting.Value + "<br>");
+           }
+           Response.Write("<strong>ConnectionStrings</strong> <br>");
+           foreach (ConnectionStringSettings setting in ConfigurationManager.ConnectionStrings)
+           {
+               Response.Write(setting.Name + "~" + setting.ConnectionString + "<br>");
+           }
+       }
